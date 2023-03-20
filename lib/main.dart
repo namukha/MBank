5f5b44f41a89c401a2d1f38cd65aaa59101f64d4
Nergui Namuun <nrgnamuunaa@gmail.com>
2023-03-20 16:05:01 +0800

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import './screens/sign_in_screen.dart';
 import 'package:bloc/bloc.dart';
 // import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 
-void main() {  
+void main() async {  
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   // WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   Bloc.observer = SimpleBlocObserver();
